af39c3b6b3c627a7351279df328e454a9a5a5a96
mcdotjs <mcdotjs@gmail.com>
2025-11-14 22:17:21 +0100

    formating

--- repo/code/win32_handmade.cpp
+++ repo/code/win32_handmade.cpp
@@ -1,108 +1,112 @@
 #include <windows.h>
 #include <winuser.h>
 
-LRESULT CALLBACK MainWindowCallback(
-    HWND Window,
-    UINT Message,
-    WPARAM WParam,
-    LPARAM LParam
-) {
-    LRESULT Result = 0;
-    switch (Message) {
-        case WM_SIZE: {
-            OutputDebugStringA("WM_SIZE");
-        }
-        break;
-        case WM_DESTROY: {
-            OutputDebugStringA("WM_DESTROY");
-        }
-        break;
-        case WM_CLOSE: {
-            OutputDebugStringA("WM_CLOSE");
-        }
-        break;
-        case WM_ACTIVATEAPP: {
-            OutputDebugStringA("WM_ACTIVATEAPP");
-        }
-        break;
-        case WM_PAINT: {
-            PAINTSTRUCT PaintStruct;
-            OutputDebugStringA("WM_PAINT");
-            //break moze byt aj inside block
-            HDC DeviceContext = BeginPaint(
-                Window, &PaintStruct
-            );
-            EndPaint(
-                Window, &PaintStruct
-            );
-            int X = PaintStruct.rcPaint.left;
-            int Y = PaintStruct.rcPaint.right;
-            int Width = PaintStruct.rcPaint.right - PaintStruct.rcPaint.left;
-            int Height = PaintStruct.rcPaint.bottom - PaintStruct.rcPaint.top;
-            //static not using in prod
-            static DWORD Operation = WHITENESS;
-            PatBlt(DeviceContext, X, Y, Width, Height, Operation);
-            if (Operation == WHITENESS) {
-                Operation = BLACKNESS;
-            } else {
-                Operation = WHITENESS;
-            }
+bool Running = false;
 
-            break;
+LRESULT CALLBACK MainWindowCallback(HWND   Window,
+                                    UINT   Message,
+                                    WPARAM WParam,
+                                    LPARAM LParam)
+{
+    LRESULT Result = 0;
+
+    switch (Message) {
+    case WM_SIZE: {
+        OutputDebugStringA("WM_SIZE");
+    } break;
+
+    case WM_DESTROY: {
+        OutputDebugStringA("WM_DESTROY");
+    } break;
+
+    case WM_CLOSE: {
+        PostQuitMessage(0);
+        OutputDebugStringA("WM_CLOSE");
+    } break;
+
+    case WM_ACTIVATEAPP: {
+        OutputDebugStringA("WM_ACTIVATEAPP");
+    } break;
+
+    case WM_PAINT: {
+        PAINTSTRUCT PaintStruct;
+        OutputDebugStringA("WM_PAINT");
+
+        // break moze byt aj inside block
+        HDC DeviceContext = BeginPaint(Window, &PaintStruct);
+        EndPaint(Window, &PaintStruct);
+        int X      = PaintStruct.rcPaint.left;
+        int Y      = PaintStruct.rcPaint.right;
+        int Width  = PaintStruct.rcPaint.right - PaintStruct.rcPaint.left;
+        int Height = PaintStruct.rcPaint.bottom - PaintStruct.rcPaint.top;
+
+        // static not using in prod
+        static DWORD Operation = WHITENESS;
+        PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+
+        if (Operation == WHITENESS) {
+            Operation = BLACKNESS;
         }
-        default: {
-            //OutputDebugStringA("WM_DESTROY");
-            Result = DefWindowProc(Window, Message, WParam, LParam);
+        else {
+            Operation = WHITENESS;
         }
+
         break;
+    }
+    default: {
+        // OutputDebugStringA("WM_DESTROY");
+        Result = DefWindowProc(Window, Message, WParam, LParam);
+    } break;
     }
     return Result;
 }
 
-int CALLBACK WinMain(
-    HINSTANCE Instance,
-    HINSTANCE PrevInstance,
-    PSTR CommandLine,
-    int ShowCode
-) {
-    WNDCLASS WindowClass = {};
-    WindowClass.style = CS_OWNDC;
-    WindowClass.lpfnWndProc = MainWindowCallback;
-    WindowClass.hInstance = Instance;
+int CALLBACK WinMain(HINSTANCE Instance,
+                     HINSTANCE PrevInstance,
+                     PSTR      CommandLine,
+                     int       ShowCode)
+{
+    WNDCLASS WindowClass      = {};
+    WindowClass.style         = CS_OWNDC;
+    WindowClass.lpfnWndProc   = MainWindowCallback;
+    WindowClass.hInstance     = Instance;
     WindowClass.lpszClassName = "HandmadeHeroWindowClass";
 
     if (RegisterClass(&WindowClass)) {
-        HWND WindowHandle =
-                CreateWindowEx(
-                    0,
-                    WindowClass.lpszClassName,
-                    "HandmadeHeroWindowClass",
-                    WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-                    CW_USEDEFAULT,
-                    CW_USEDEFAULT,
-                    CW_USEDEFAULT,
-                    CW_USEDEFAULT,
-                    0,
-                    0,
-                    Instance,
-                    0
-                );
+        HWND WindowHandle = CreateWindowEx(0,
+                                           WindowClass.lpszClassName,
+                                           "HandmadeHeroWindowClass",
+                                           WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                                           CW_USEDEFAULT,
+                                           CW_USEDEFAULT,
+                                           CW_USEDEFAULT,
+                                           CW_USEDEFAULT,
+                                           0,
+                                           0,
+                                           Instance,
+                                           0);
         if (WindowHandle) {
             MSG Message;
             for (;;) {
                 BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if (Running) {
+                    break;
+                }
                 if (MessageResult > 0) {
                     TranslateMessage(&Message);
                     DispatchMessage(&Message);
-                } else {
+                }
+                else {
                     break;
                 }
             }
-        } else {
-            //TODO: logging
         }
-    } else {
-        //TODO: logging?
+        else {
+            // TODO: logging
+        }
+    }
+    else {
+        // TODO: logging?
     }
     return 0;
 }
