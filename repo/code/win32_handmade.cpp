864c866370ad16a57da16796c7a56ad14a5ae17d
mcdotjs <mcdotjs@gmail.com>
2025-11-30 14:29:56 +0100

    ansi A?

--- repo/code/win32_handmade.cpp
+++ repo/code/win32_handmade.cpp
@@ -76,12 +76,12 @@ global_variable x_input_set_state *XInputSetState_ = XInputSetStateStub;
 
 internal void Win32LoadXInput(void)
 {
-    HMODULE XInputLibrary = LoadLibrary("xinput1_4.dll");
+    HMODULE XInputLibrary = LoadLibraryA("xinput1_4.dll");
     if (XInputLibrary) {
-        XInputGetState = (x_input_get_state *) GetProcAddress(XInputLibrary,
-                                                              "XInputGetState");
-        XInputSetState = (x_input_set_state *) GetProcAddress(XInputLibrary,
-                                                              "XInputSetState");
+        XInputGetState = (x_input_get_state *) GetProcAddress(
+            XInputLibrary, "XInputGetState");
+        XInputSetState = (x_input_set_state *) GetProcAddress(
+            XInputLibrary, "XInputSetState");
     }
 }
 /*
@@ -320,7 +320,7 @@ int CALLBACK WinMain(HINSTANCE Instance,
     // NOTE: stack overflow
     // uint8 BigOldHeavyBlockOfMemory[ 2 * 1024 * 1024] = {};
     Win32LoadXInput();
-    WNDCLASS WindowClass = {};
+    WNDCLASSA WindowClass = {};
     Win32ResizeDIBSection(&GlobalBackBuffer, 1280, 720);
     WindowClass.style = CS_HREDRAW | CS_VREDRAW
                         | CS_OWNDC; // when resizing window refresh whole
